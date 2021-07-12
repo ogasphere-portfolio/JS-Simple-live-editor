25afd637f758b4c6edfa8e17e57048b24603917f
Christophe Deneuve <33314621+christopheOclock@users.noreply.github.com>
2021-07-12 11:29:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# _Simple Live Editor_
+
+Votre but : permettre à l'utilisateur du site de saisir du texte, y compris du code HTML, dans une zone de saisie :thinking:
+
+Et là :pray: à chaque touche du clavier tapé, le contenu de la zone de saisie est « injecté » dans une div, sous l'éditeur.  
+L'éditeur prend vie :unicorn:
+
+![Résultat attendu](resultat.png)
+
+En résumé, on va créer un éditeur HTML en temps réel (lol).
+
+## Instructions
+
+* Importer le fichier index.js dans index.html :nerd_face:
+* Suivre les étapes indiquées dans index.js :muscle:
---
+++ index.js
@@ -0,0 +1,22 @@
+// Stocker dans des variables les éléments HTML sur lesquels agir :
+// - la balise textarea#user-input
+// - la balise div#contenuHTML
+
+
+
+// Créer une fonction à appeler dès qu'une touche sera relachée dans la zone de
+// saisie #user-input. Cette fonction doit remplacer le contenu de #contenuHTML
+// par celui de la textarea, mais attention : le *texte* de #input-text doit
+// devenir du *HTML* dans #contenuHTML
+
+
+
+// Appeler la fonction lors d'un événement 'keyup'. Si tout va bien… ça marche !
+
+
+
+// Bonus : pré-remplir #user-input avec le contenu de #contenuHTML, pour donner
+// un exemple à l'utilisateur dès que la page se charge.
+// - Ajouter du contenu HTML dans #contenuHTML (en dur, ou via JS).
+// - Récupérer ce HTML sous forme texte et l'utiliser comme valeur de la #user-input.
+
